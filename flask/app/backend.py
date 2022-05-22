eb0c3f9a1945f0da56f002114e48210315f0a067
Zeyrux <tappetheo@gmail.com>
2022-05-22 19:18:22 +0200

    added flask socket io

--- flask/app/backend.py
+++ flask/app/backend.py
@@ -44,6 +44,16 @@ if os.path.isdir(SONG_DIR):
     shutil.rmtree(SONG_DIR)
 
 
+def download_cur_song(token_info) -> tuple["Track", "TokenManager"]:
+    token_manager = TokenManager(token_info, spotify_api)
+    track = spotify_api.get_currently_playing_track(
+        token_manager.get_access_token(), token_manager.get_token_type()
+    )
+    pytube_obj = youtube_api.search_song(track)
+    youtube_api.download(pytube_obj, track.get_filename())
+    return track, token_manager
+
+
 @app.before_first_request
 def start():
     spotify_api.set_redirect_uri(url_for("save_login", _external=True))
@@ -70,14 +80,14 @@ def save_login():
     return redirect(url_for("visualizer"))
 
 
+@app.route("/skip", methods=["GET"])
+def skip():
+    pass
+
+
 @app.route("/visualizer", methods=["GET"])
 def visualizer():
     if not session.get("token_info", None):
         return redirect(url_for("homepage"))
-    token_manager = TokenManager(session["token_info"], spotify_api)
-    track = spotify_api.get_currently_playing_track(
-        token_manager.get_access_token(), token_manager.get_token_type()
-    )
-    pytube_obj = youtube_api.search_song(track)
-    youtube_api.download(pytube_obj, track.get_filename())
+    track, token_manager = download_cur_song(session["token_info"])
     return render_template("visualizer.html", filename=track.get_filename())
--- flask/run.py
+++ flask/run.py
@@ -2,13 +2,16 @@ import mimetypes
 
 from app.backend import app
 
+from flask_socketio import SocketIO
+
 
 mimetypes.add_type('application/javascript', '.js')
 
 
 def main():
     app.env = "development"
-    app.run(debug=True, host="localhost", port=5000)
+    socket_io = SocketIO(app)
+    socket_io.run(app, host="localhost", port=5000)
 
 
 if __name__ == '__main__':
