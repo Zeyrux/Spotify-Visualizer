ad64eb707dc8c284cf81d7bb65dd2ae2c6bacfd3
Zeyrux <tappetheo@gmail.com>
2022-06-22 09:46:26 +0200

    added user playlist support for javascript

--- flask/app/static/scritps/init.js
+++ flask/app/static/scritps/init.js
@@ -1,5 +1,11 @@
+import { Playlist } from "./tracks.js";
+
 export var controller = JSON.parse(document.getElementById("init_script").getAttribute("controller"));
 export var user_playlists = JSON.parse(document.getElementById("init_script").getAttribute("user_playlists"));
+for (let i = 0; i < user_playlists.length; i++) {
+    user_playlists[i] = new Playlist(user_playlists[i]);
+    
+}
 console.log(user_playlists);
 
 
